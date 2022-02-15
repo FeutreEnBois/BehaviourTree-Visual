ff5187c3123da04eee2bb2e8330d420702efd95b
Maxime Derosier <derosier.maxime@gmail.com>
2022-02-15 20:11:52 +0100

    node can now visualy link together

--- Visual_More_Advanced_Behaviour_v2/Assets/Editor/BehaviourTreeView.cs
+++ Visual_More_Advanced_Behaviour_v2/Assets/Editor/BehaviourTreeView.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor.Experimental.GraphView;
 using System;
+using System.Linq;
 
 public class BehaviourTreeView : GraphView
 {
-
     public new class UxmlFactory : UxmlFactory<BehaviourTreeView, GraphView.UxmlTraits> { }
+
+    BehaviourTree tree;
     public BehaviourTreeView()
     {
         Insert(0, new GridBackground());
@@ -24,6 +27,75 @@ public class BehaviourTreeView : GraphView
 
     internal void PopulateView(BehaviourTree tree)
     {
-        throw new NotImplementedException();
+        this.tree = tree;
+
+        graphViewChanged -= OnGraphViewChanged;
+        DeleteElements(graphElements);
+        graphViewChanged += OnGraphViewChanged;
+
+        tree.nodes.ForEach(n => CreateNodeView(n));
+    }
+
+    public override List<Port> GetCompatiblePorts(Port startPort, NodeAdapter nodeAdapter)
+    {
+        return ports.ToList().Where(endPort =>
+        endPort.direction != startPort.direction &&
+        endPort.node != startPort.node).ToList();
+    }
+
+    private GraphViewChange OnGraphViewChanged(GraphViewChange graphViewChange)
+    {
+        if(graphViewChange.elementsToRemove != null)
+        {
+            graphViewChange.elementsToRemove.ForEach(e =>
+            {
+                NodeView nodeView = e as NodeView;
+                if(nodeView != null)
+                {
+                    tree.DeleteNode(nodeView.node);
+                }
+            });
+        }
+        return graphViewChange;
+    }
+
+    public override void BuildContextualMenu(ContextualMenuPopulateEvent evt)
+    {
+        //base.BuildContextualMenu(evt);
+        {
+            var types = TypeCache.GetTypesDerivedFrom<ActionNode>(); // get all nodes derived from an action nodes <3
+            foreach (var type in types)
+            {
+                evt.menu.AppendAction($"[{type.BaseType.Name}] {type.Name}", (a) => CreateNode(type));
+            }
+        }
+
+        {
+            var types = TypeCache.GetTypesDerivedFrom<CompositeNode>(); // get all nodes derived from an composite nodes <3
+            foreach (var type in types)
+            {
+                evt.menu.AppendAction($"[{type.BaseType.Name}] {type.Name}", (a) => CreateNode(type));
+            }
+        }
+
+        {
+            var types = TypeCache.GetTypesDerivedFrom<DecoratorNode>(); // get all nodes derived from an decorator nodes <3
+            foreach (var type in types)
+            {
+                evt.menu.AppendAction($"[{type.BaseType.Name}] {type.Name}", (a) => CreateNode(type));
+            }
+        }
+    }
+
+    private void CreateNode(System.Type type)
+    {
+        Node node = tree.CreateNode(type);
+        CreateNodeView(node);
+    }
+
+    void CreateNodeView(Node node)
+    {
+        NodeView nodeView = new NodeView(node);
+        AddElement(nodeView);
     }
 }
